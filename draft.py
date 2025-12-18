b900e7574cf4f86d9ab2da91bfc4a60ed834a99e
KhangDS&DL <gausseuler159357@gmail.com>
2025-12-18 19:28:47 +0700

    modularize my 1st func

--- draft.py
+++ draft.py
@@ -1,7 +1,7 @@
 print("Hello World")
 #
-def add(a, b):
-    return a + b    
+def calculate_total(items:List[float], tax_rate:float)->float:
+    return (sum(items) * (1 + tax_rate))
 
-print(add(1, 2))
+print(calculate_total([1, 2], 0.1))
     
